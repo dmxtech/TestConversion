ac42825efd236bf2ca2f2580bb65ad80b1b5557d
Javier <dmxtechinn@gmail.com>
2023-02-21 15:02:37 -0600

    first commit, testing double and int using different variables

---
+++ test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'findLongestSubsequence' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    let arr = [];
+
+    for (let i = 0; i < arrCount; i++) {
+        const arrItem = parseInt(readLine().trim(), 10);
+        arr.push(arrItem);
+    }
+
+    const result = findLongestSubsequence(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
