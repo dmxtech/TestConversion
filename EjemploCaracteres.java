2942ba024493b5e2033dedb269a3f698962cf5d3
Javier <dmxtechinn@gmail.com>
2023-02-21 15:53:41 -0600

    testing with characters

---
+++ EjemploCaracteres.java
@@ -0,0 +1,14 @@
+public class EjemploCaracteres {
+
+    public static void main(String[] args) {
+        String cuota1 = "10";
+        String cuota2 = "20";
+        System.out.println(cuota1 + cuota2);
+
+        String saludo = "Hola, mi nombre es ";
+        String nombre = "Rómulo ";
+        String continuacion = "y mi edad es ";
+        int edad = 100;
+        System.out.println(saludo + nombre + continuacion + edad);
+    }
+}
